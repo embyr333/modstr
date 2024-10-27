fea0ea455d2744b739c6e9f3ea5b601f9ad32290
embyr333 <emb333@gmail.com>
2024-10-27 09:40:53 +0000

    10th commit - removed the console-print statements & added a few more keywords

--- modstr.py
+++ modstr.py
@@ -1,13 +1,12 @@
-# 9th commit
+# 10th commit - removed the console-print statements & added a few more keywords
 
 def upper_SQL_keywords(s: str): # Note1
     if s == '':
-        # ---now also returning string so can appear in GUI output
         no_input_message = 'Please enter the query string you want to process'
-        print(no_input_message)
         return no_input_message
 
-    key_set = ('select', 'from', 'where', 'is', 'and', 'null') # TODO continue to extend as needed
+    key_set = ('select', 'from', 'where', 'is', 'and', 'null', 'like', 'in', 'between', 'and') 
+    # ... TODO continue to extend as needed
 
     semicolon = False
     if s[-1] == ';':
@@ -36,51 +35,10 @@ def upper_SQL_keywords(s: str): # Note1
     if semicolon == True:
         new_string = new_string.rstrip() + ';'
 
-    print(new_string) # (*joining on quote mark here would be problematic as need extra space upstream or downstream)
-    return new_string # ---now also returning string so can appear in GUI output
+    return new_string 
 
 
-print('Example1') 
-upper_SQL_keywords("select first_name, last_name, gender FROM patients where gender is 'M';")
-# SELECT first_name, last_name, gender FROM patients WHERE gender IS 'M';
-
-print('\nExample2: a value in quotation marks has a word from keyword that should not be processed as a keyword') 
-upper_SQL_keywords("select first_name, last_name, gender from patients where gender is 'M is gender';")
-# SELECT first_name, last_name, gender FROM patients WHERE gender IS 'M is gender';
-print()
-
-print('Example3 - keyword adjacent to (terminal) semicolon') 
-upper_SQL_keywords("select first_name, last_name from patients where allergies is null;")
-# SELECT first_name, last_name FROM patients WHERE allergies IS NULL;
-print()
-
-print('Example4 (no semicolon)') 
-upper_SQL_keywords("select first_name, last_name from patients where allergies is null")
-# SELECT first_name, last_name FROM patients WHERE allergies IS NULL
-print()
-# TODO: Also, should consider if want to allow processing of muliple lines...
-
-# Can paste input between the (double) quotes here...
-upper_SQL_keywords("")
-print()
-
-# TODO: Further test cases will be tried as I use it (and of course add further items to key_set)
-# TODO: Consider making a GUI
-
-'''
-Note1: Using the optional arg type specifier has the advantage of allowing
-IDE to make builtin method suggestions on typing dot after string etc.
-
-Note2: Much of the code is dealing with possibility of words from key_set used within a string in query, 
-i.e. NOT as keywords, so would NOT want to mod, e.g. 2nd 'is' in the following...
-select first_name, last_name, gender from patients where gender is 'M is gender';
-[Iteration 4 was dead end with shelx.split() - does not preserve quotation marks]
-The whole thing is now rather messy, with some (inefficient) string concatenation, 
-and might be better tried with regex, but this approach is probably good enough for now.
-'''
-
-
-# ---Adding a GUI for flexible input-output...
+# GUI...
 
 from tkinter import *
 root_widget = Tk()
@@ -109,3 +67,33 @@ output_field = Entry(root_widget, width = 100, fg = 'blue', textvariable=output,
 output_field.grid(row=3, column=0) # (located on 4th row)
 
 root_widget.mainloop()
+
+
+'''
+Note1: Using the optional arg type specifier has the advantage of allowing
+IDE to make builtin method suggestions on typing dot after string etc.
+
+Note2: Much of the code is dealing with possibility of words from key_set used within a string in query, 
+i.e. NOT as keywords, so would NOT want to mod, e.g. 2nd 'is' in the following...
+select first_name, last_name, gender from patients where gender is 'M is gender';
+[Iteration 4 was dead end with shelx.split() - does not preserve quotation marks]
+The whole thing is now rather messy, with some (inefficient) string concatenation, 
+and might be better tried with regex, but this approach is probably good enough for now.
+
+
+Example1
+select first_name, last_name, gender FROM patients where gender is 'M';
+SELECT first_name, last_name, gender FROM patients WHERE gender IS 'M';
+
+Example2 - a value in quotation marks has a word from keyword that should not be processed as a keyword
+select first_name, last_name, gender from patients where gender is 'M is gender';
+SELECT first_name, last_name, gender FROM patients WHERE gender IS 'M is gender';
+
+Example3 - keyword adjacent to (terminal) semicolon
+select first_name, last_name from patients where allergies is null;
+SELECT first_name, last_name FROM patients WHERE allergies IS NULL;
+
+Example4 - no semicolon
+select first_name, last_name from patients where allergies is null
+SELECT first_name, last_name FROM patients WHERE allergies IS NULL
+'''
